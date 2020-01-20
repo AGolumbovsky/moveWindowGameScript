87314677faf4bafdf3a9870c467b6e75e7703a11
agolumbovsky <agolumbovsky@gmail.com>
2020-01-19 20:26:21 -0500

    first nax

---
+++ moveWindowGameScript.js
@@ -0,0 +1,29 @@
+
+let openNewWi = () => {
+
+    theWindow = window.open('', '', 'width=200, height=100');
+    theWindow.document.write('<h3><b>Close me if you can...</b></h3>');
+
+}
+  
+let moveWi = () => {
+
+    if (theWindow) {
+
+        let randomHorizontal = Math.floor(Math.random() * window.innerWidth);
+        let randomVertical = Math.floor(Math.random() * window.innerHeight);
+        
+        theWindow.moveTo(randomHorizontal, randomVertical);
+        theWindow.focus();
+        
+    }
+    else theWindow.close();
+}
+
+openNewWi();
+
+setInterval(() => {
+
+        moveWi()
+
+}, 2000);
